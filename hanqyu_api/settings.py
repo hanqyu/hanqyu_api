e4729369da7fbab51a5027fd5f0fb2eaeb2523b2
hanqyu <peakjnut37@gmail.com>
2019-03-26 04:01:23 +0900

    add logging

--- hanqyu_api/settings.py
+++ hanqyu_api/settings.py
@@ -150,3 +150,22 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
