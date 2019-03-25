bf56fd9697d75da86376c3413becf752a8c245b5
hanqyu <peakjnut37@gmail.com>
2019-03-26 04:50:13 +0900

    add ALLOWED_HOSTS

--- hanqyu_api/settings.py
+++ hanqyu_api/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'api-test.hanqyu.com',
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'api-test.hanqyu.com',
                  'ec2-13-209-111-182.ap-northeast-2.compute.amazonaws.com', '13.209.111.182']
 
 
